6b01c8fad569981a46eccab1271fae5dda8e3405
LSabnani <sabnani@gmail.com>
2024-02-10 17:32:28 +0000

    Updated front end back end communication

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    fastAPIUrl: 'https://monikakusumanc-reactapp-b0pbbzqjaoo.ws-us108.gitpod.io'
+    fastAPIUrl: 'https://8000-lsabnani-llmfirstapi-i9fcl25iixx.ws-us108.gitpod.io/get_gemini_completion'
   };
--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    fastAPIUrl: 'https://lsabnani-llmfirstapi-g8iiobviupf.ws-us108.gitpod.io'
+    fastAPIUrl: 'https://8000-lsabnani-llmfirstapi-i9fcl25iixx.ws-us108.gitpod.io/get_gemini_completion'
   };
